2b5300055b79c95ced64fc0d8307134c117970ab
Jay Bednar <ironviralmedia@gmail.com>
2018-02-07 18:46:56 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,9 @@
 # Add  code here!
 def prime?(num)
-
   if num < 2
     false
   else
     my_range = (2..((num ** 0.5).ceil)).to_a
-    my_range.none? {|i| num % i.to_f == 0 if num != 2}
+    my_range.none? {|i| num == 2 ? true : num % i.to_f == 0 }
   end
 end
